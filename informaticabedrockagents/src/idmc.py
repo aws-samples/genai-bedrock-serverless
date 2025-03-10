9d764397f9c9aa8c218c852429c5b11bbad813bc
Mahajan <kmmahaj@amazon.com>
2025-03-10 11:51:06 -0700

    new api schema

--- informaticabedrockagents/src/idmc.py
+++ informaticabedrockagents/src/idmc.py
@@ -1,130 +1,56 @@
 import json
 import boto3
-import sqlite3
-from datetime import datetime
-import random
+import requests
 import logging
 import os
-import csv
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-s3 = boto3.client('s3')
-bucket = os.environ.get('BUCKET_NAME')  #Name of bucket with data file and OpenAPI file
-cw_log_file = 'applogs.csv' #Location of data file in S3
-local_db = '/tmp/applogs.csv' #Location in Lambda /tmp folder where data file will be copied
-
-#Download data file from S3
-s3.download_file(bucket, cw_log_file, local_db)
-
-cursor = None
-conn = None
-
-applogs_db = '/tmp/applogs.db'
-
-#Initial data load and SQLite3 cursor creation 
-def load_data():
-    # create db
-    global conn
-    conn = sqlite3.connect(applogs_db)
-    cursor = conn.cursor()
-    logger.info('Completed initial data load ')
-
-    # Creating table 
-    table ="""CREATE TABLE AppLogsDB(HTTPErrorCode VARCHAR(255), Timestamp VARCHAR(255), 
-    Description VARCHAR(255));"""
-    cursor.execute(table) 
-
-# Queries to INSERT records. 
-    cursor.execute('''INSERT INTO AppLogsDB VALUES ('500', '202404219:00', 'DBClusterRoleAlreadyExists')''')
-    cursor.execute('''INSERT INTO AppLogsDB VALUES ('400', '202404220:00', 'DBClusterRoleQuotaExceeded')''') 
-    cursor.execute('''INSERT INTO AppLogsDB VALUES ('400', '202404221:00', 'InvalidDBClusterStateFault')''') 
-   
-# Display data inserted 
-    print("Data Inserted in the table: ") 
-    data=cursor.execute('''SELECT * FROM AppLogsDB''') 
-    for row in data: 
-	    print(row) 
-
-# Commit your changes in the database	 
-    conn.commit() 
-
-    return cursor
-    
-    #Initial data load and SQLite3 cursor creation 
-def load_data_s3():
-    # create db
-    global conn
-    conn = sqlite3.connect(applogs_db)
-    cursor = conn.cursor()
-    logger.info('Completed initial data load ')
-
-    # Creating table 
-    table ="""CREATE TABLE AppLogsDB(HTTPErrorCode VARCHAR(255), Timestamp VARCHAR(255), 
-    Description VARCHAR(255));"""
-    cursor.execute(table) 
-
-# Load data file. 
-    with open('/tmp/applogs.csv', 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-        # Insert the data into the table
-            cursor.execute('''INSERT INTO AppLogsDB (HTTPErrorCode, Timestamp, Description) VALUES (?,?,?)''', row)
-   
-# Display data inserted 
-    print("Data Inserted in the table: ") 
-    data=cursor.execute('''SELECT * FROM AppLogsDB''') 
-    for row in data: 
-	    print(row) 
-
-# Commit your changes in the database	 
-    conn.commit() 
-
-    return cursor
-    
-    
-#Function returns error details based on error date/time and high level description
-def get_error_description(httperrorcode, timestamp):
+def invoke_aws_rag_agent(prompt):
   
-    data = cursor.execute("select Description from AppLogsDB where HTTPErrorCode like ? and Timestamp like ?", ("%" + httperrorcode + "%", "%" + timestamp + "%"))
-    #for row in data: 
-	#    print(row) 
-    error_description = cursor.fetchone()[0]
-    print("error_description is: ", error_description)
-    return error_description
-  
+    url = "https://usw1-cai.dmp-us.informaticacloud.com:443/active-bpel/public/rt/37gfTBWcbO7fgYmIsNrMkl/AWSRAGAgent"
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+    try:
+        response = requests.get(url, params={"prompt": prompt}, headers=headers)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+        data = response.json()  # Assuming the response is in JSON format
+        return data.get("enterprise_information", {}).get("UberPlannerPO", {}).get("Planner", {}).get("executor_response", "No executor response found")
+    
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error fetching agent response: {e}")
+        return {"error": "Failed to fetch agent response"}
+    
 
 def lambda_handler(event, context):
  
-    global cursor
-    if cursor == None:
-        cursor = load_data_s3()
     
-    timestamp = ''
-    httperrorcode = ''
+    prompt = ''
+   
     api_path = event['apiPath']
     logger.info('API Path')
     logger.info(api_path)
     
-    if api_path == '/get_error_description':
+    if api_path == '/AWSRAGAgent':
         parameters = event['parameters']
         print ("parameters are: ", parameters)
         
         for parameter in parameters:
-            if parameter["name"] == "timestamp":
-                timestamp = parameter["value"]
-            if parameter["name"] == "httperrorcode":
-                httperrorcode = parameter["value"]
+            if parameter["name"] == "prompt":
+                prompt = parameter["value"]
                 
     #    logger.info(timestamp)
     #    logger.info(httperrorcode)
     #    timestamp =  event['parameters']['timestamp']
     #    httperrorcode = event['parameters']['httperrorcode']
-        logger.info(timestamp)
-        logger.info(httperrorcode)
-        body = get_error_description(httperrorcode, timestamp)
+        logger.info(prompt)
+   
+        body = invoke_aws_rag_agent(prompt)
     else:
         body = {"{} is not a valid api, try another one.".format(api_path)}
     
